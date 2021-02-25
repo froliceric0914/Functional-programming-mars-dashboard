a738ac76f2519e55e47b182b0edcb6f9c2f0a0e5
froliceric0914 <wzhao.eric@gmail.com>
2021-02-24 22:38:15 -0500

    feat: add get rover api

--- src/server/index.js
+++ src/server/index.js
@@ -13,6 +13,19 @@ app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
+app.get('/rovers/:rover_name', async (req, res) => {
+    try {
+        const earth_date = req.query.max_date;
+        console.log('date: ', earth_date);
+        let rovers_img = await fetch(
+            `https://api.nasa.gov/mars-photos/api/v1/rovers/${req.params.rover_name}/photos?earth_date=${earth_date}&api_key=${process.env.API_KEY}`
+        ).then((res) => res.json());
+        console.log('hello world');
+        res.send(rovers_img);
+    } catch (err) {
+        console.log('error: ', err);
+    }
+});
 
 // example API call
 app.get('/apod', async (req, res) => {
