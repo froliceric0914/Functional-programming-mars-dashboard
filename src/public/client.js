696c3717e7a676a5fd103abf8342320d9a615f91
froliceric0914 <wzhao.eric@gmail.com>
2021-02-24 21:26:31 -0500

    feat: add API Key and render the first image

--- src/public/client.js
+++ src/public/client.js
@@ -1,25 +1,24 @@
 let store = {
-    user: { name: "Student" },
+    user: { name: 'Student' },
     apod: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+};
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+    store = Object.assign(store, newState);
+    render(root, store);
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
-
+    root.innerHTML = App(state);
+};
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
+    let { rovers, apod } = state;
 
     return `
         <header></header>
@@ -40,13 +39,13 @@ const App = (state) => {
             </section>
         </main>
         <footer></footer>
-    `
-}
+    `;
+};
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
-})
+    render(root, store);
+});
 
 // ------------------------------------------------------  COMPONENTS
 
@@ -55,51 +54,52 @@ const Greeting = (name) => {
     if (name) {
         return `
             <h1>Welcome, ${name}!</h1>
-        `
+        `;
     }
 
     return `
         <h1>Hello!</h1>
-    `
-}
+    `;
+};
 
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (apod) => {
-
+    console.log('apod', apod);
     // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
+    const today = new Date();
+    const photodate = new Date(apod.date);
     console.log(photodate.getDate(), today.getDate());
 
     console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
+    if (!apod || apod.date === today.getDate()) {
+        getImageOfTheDay(store);
     }
 
     // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
+    if (apod.media_type === 'video') {
+        return `
             <p>See today's featured video <a href="${apod.url}">here</a></p>
             <p>${apod.title}</p>
             <p>${apod.explanation}</p>
-        `)
+        `;
     } else {
-        return (`
+        return `
             <img src="${apod.image.url}" height="350px" width="100%" />
             <p>${apod.image.explanation}</p>
-        `)
+        `;
     }
-}
+};
 
 // ------------------------------------------------------  API CALLS
 
 // Example API call
 const getImageOfTheDay = (state) => {
-    let { apod } = state
+    let { apod } = state;
+    console.log('state', state);
 
     fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        .then((res) => res.json())
+        .then((apod) => updateStore(store, { apod }));
 
-    return data
-}
+    return data;
+};
