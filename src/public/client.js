d0ec438c8faeb23315763d357997503e29dba2c2
froliceric0914 <wzhao.eric@gmail.com>
2021-03-07 17:21:18 -0500

    refactor: move the function declaration before calling && add status to rover

--- src/public/client.js
+++ src/public/client.js
@@ -1,4 +1,8 @@
-// const { update } = require('immutable');
+//Build custom pure functions to do logic. No logic should happen outside of these functions.
+/*
+Add the Mars weather embed code.
+Add a wind graph at the location of the rover.
+*/
 const { Map, List, set } = require('immutable');
 
 let store = Map({
@@ -17,35 +21,33 @@ const getRoverData = (rover_name) => {
     fetch(`http://localhost:3000/rovers/${rover_name}`)
         .then((res) => res.json())
         .then(({ photo_manifest }) => {
+            console.log('photo_manifest', photo_manifest);
             updateStore(store, {
                 rovers: set(store.toJS().rovers, rover_name, {
                     launch_date: photo_manifest.launch_date,
                     landing_date: photo_manifest.landing_date,
                     name: photo_manifest.name,
+                    status: photo_manifest.status, //need to add
                 }),
             });
         });
 };
 
-const getLatestRoverPhotos = async (rover_name) => {
-    try {
-        fetch(`http://localhost:3000/rover_photo/${rover_name}`)
-            .then((res) => res.json())
-            .then((data) => {
-                const trimPhoto = data.latest_photos.slice(0, 1);
-                updateStore(store, {
-                    rovers: {
-                        [rover_name]: set(
-                            store.toJS().rovers[rover_name],
-                            'photo',
-                            trimPhoto
-                        ),
-                    },
-                });
+const getLatestRoverPhotos = (rover_name) => {
+    fetch(`http://localhost:3000/rover_photo/${rover_name}`)
+        .then((res) => res.json())
+        .then((data) => {
+            const trimPhoto = data.latest_photos.slice(0, 1);
+            updateStore(store, {
+                rovers: {
+                    [rover_name]: set(
+                        store.toJS().rovers[rover_name],
+                        'photo',
+                        trimPhoto
+                    ),
+                },
             });
-    } catch (errr) {
-        console.log('get latest rover photos error', err);
-    }
+        });
 };
 
 // add our markup to the page
@@ -54,6 +56,8 @@ const root = document.getElementById('root');
 const render = async (root, state) => {
     root.innerHTML = App(state);
 };
+
+// ------------------------------------------------------  COMPONENTS
 
 // update the current rover by selecting on Navbar item
 function onSelectNav(rover) {
@@ -75,30 +79,6 @@ const NavBar = (roverNames, selectedRover) => {
     </nav>
     `;
 };
-
-const App = (state) => {
-    let { roverNames, rovers, selectedRover } = state.toJS();
-
-    return `
-        <header>${NavBar(roverNames, selectedRover)}</header>
-            <main>
-            <h1 class="greeting">Welcome to check the general infomation of Mars rovers</h1>
-            <section>
-                ${RoverData(rovers, selectedRover)}
-            </section>
-                </main>
-        <footer>
-        <p>This website is made by <span id="heart">&#9829;</span> and based on NASA open API service (<a href="https://api.nasa.gov/" target="_blank">https://api.nasa.gov/)</a></p> 
-        <p>Developed and maintainedby Eric Zhao(github id: <a href="https://github.com/froliceric0914" target="_blank">froliceric0914</a>)</p>
-        </footer>`;
-};
-
-// listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store);
-});
-
-// ------------------------------------------------------  COMPONENTS
 
 const LatestRoverPhoto = (rover_name, rovers) => {
     const selectedRoverPhotos = rovers[rover_name].photo;
@@ -151,9 +131,32 @@ const RoverData = (rovers, selectedRover) => {
             <div class="rover-info">
                 ${LatestRoverPhoto(selectedRover, rovers)}
                 <p>Rover Name: ${rover}</p>
+                <p>Status: ${roverToRender.status}</p>
                 <p>Launch date: ${roverToRender.launch_date}</p>
                 <p>Landing date: ${roverToRender.landing_date}</p>
             </div>
         </div>
     </section>`;
 };
+
+const App = (state) => {
+    let { roverNames, rovers, selectedRover } = state.toJS();
+
+    return `
+        <header>${NavBar(roverNames, selectedRover)}</header>
+            <main>
+            <h1 class="greeting">Welcome to check the general infomation of Mars rovers</h1>
+            <section>
+                ${RoverData(rovers, selectedRover)}
+            </section>
+                </main>
+        <footer>
+        <p>This website is made by <span id="heart">&#9829;</span> and based on NASA open API service (<a href="https://api.nasa.gov/" target="_blank">https://api.nasa.gov/)</a></p> 
+        <p>Developed and maintainedby Eric Zhao(github id: <a href="https://github.com/froliceric0914" target="_blank">froliceric0914</a>)</p>
+        </footer>`;
+};
+
+// listening for load event because page should load before any JS is called
+window.addEventListener('load', () => {
+    render(root, store);
+});
